087e9b62eb87edb6975fdeae3637fd9d8fff8cac
Sreeahk02 <sreeahk@gmail.com>
2023-11-04 17:29:08 +0530

    first_draft

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def root():
-    return "Hello,My name is Sreeahk"
+    return render_template("landing.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 
 if __name__ == "__app__":
